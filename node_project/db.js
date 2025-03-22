7b0688833b0ad6faa0149d332943dc82ae46236c
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2025-03-22 10:00:59 -0400

    Something working
    I currently have it working to a slight extent. You can docker compose up --build and see all three containers running. Problematically, once you "submit" a new shark, it crashes the app, but this is progress.
    
    I was encountering tons of trouble locally; I don't really want to talk about it. Basically, the fix was to use the latest version of the node alpine image by docker. The Dockerfile from the tutorial repo needed to be modified (removed the USER node line) because I couldn't npm install mongoose with the newer alpine version.

--- node_project/db.js
+++ node_project/db.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const MONGO_USERNAME = 'strata';
 const MONGO_PASSWORD = 'labs';
-const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_HOSTNAME = 'mongo';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'sharkinfo';
+
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+mongoose.connect(url, {useNewUrlParser: true});
