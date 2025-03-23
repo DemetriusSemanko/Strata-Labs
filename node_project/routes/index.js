fc66fa83e33f77f3f7a6d7a4141d90a6fe7369f9
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2025-03-23 19:30:43 -0400

    Switched node app to use port 8080
    I just wanted this to follow the tutorial as much as possible.

--- node_project/routes/index.js
+++ node_project/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.use (function (req,res,next) {
+router.use(function (req,res,next) {
     console.log('/' + req.method);
     next();
 });
