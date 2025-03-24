6bcf09815e2bd139b4a15c4ba6a70aba726a708c
Demetrius Semanko <143662059+DemetriusSemanko@users.noreply.github.com>
2025-03-23 20:05:33 -0400

    App works finally
    I had to replace all the functions that used callbacks with ones that used async/await. Things are kosher.

--- node_project/controllers/sharks.js
+++ node_project/controllers/sharks.js
@@ -5,23 +5,32 @@ exports.index = function (req, res) {
     res.sendFile(path.resolve('views/sharks.html'));
 };
 
+/*
 exports.create = function (req, res) {
     var newShark = new Shark(req.body);
     console.log(req.body);
-    newShark.save()
-        .then(() => res.redirect('/sharks'))
-        .catch(err => res.status(500).send(err));
-    /*
     newShark.save(function (err) {
-        if (err) {
+        if(err) {
             res.status(400).send('Unable to save shark to database');
         } else {
             res.redirect('/sharks/getshark');
         }
     });
-    */
+});
+*/
+
+exports.create = async function (req, res) {
+    try {
+        var newShark = new Shark(req.body);
+        console.log(req.body);
+        await newShark.save();  // Use await to save the shark
+        res.redirect('/sharks/getshark');  // Redirect if successful
+    } catch (err) {
+        res.status(400).send('Unable to save shark to database');  // Handle error
+    }
 };
 
+/*
 exports.list = function (req, res) {
     Shark.find({}).exec(function (err, sharks) {
         if (err) {
@@ -31,4 +40,16 @@ exports.list = function (req, res) {
             sharks: sharks
         });
     });
+};
+*/
+
+exports.list = async function (req, res) {
+    try {
+        const sharks = await Shark.find({}).exec();  // Use await to get sharks
+        res.render('getshark', {
+            sharks: sharks
+        });
+    } catch (err) {
+        res.status(500).send(err);  // Handle errors with status 500
+    }
 };
